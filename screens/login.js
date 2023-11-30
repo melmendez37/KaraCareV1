9fa4d4fe72d7671e1d7709941e2d0cbd1568d62e
changchuuu <89124292+changchuuu@users.noreply.github.com>
2023-11-30 09:37:52 +0800

    Add files via upload

---
+++ screens/login.js
@@ -0,0 +1,508 @@
+/*
+ *	This content is generated from the PSD File Info.
+ *	(Alt+Shift+Ctrl+I).
+ *
+ *	@desc 		
+ *	@file 		login
+ *	@date 		Tuesday 28th of November 2023 12:05:25 PM
+ *	@title 		Page 1
+ *	@author 	
+ *	@keywords 	
+ *	@generator 	Export Kit v1.3.figma
+ *
+*/
+
+var content_container = document.createElement("div");
+content_container.style.width = "360px";
+content_container.id = "content_container";
+content_container.style.position = "relative";
+content_container.style.display = "block";
+content_container.style.margin = "0 auto";
+document.body.appendChild(content_container);
+
+var _bg__login = document.createElement("div");
+_bg__login.id = "_bg__login";
+_bg__login.style.left = "0px";
+_bg__login.style.top = "0px";
+_bg__login.style.width = "360px";
+_bg__login.style.height = "800px";
+_bg__login.style.borderRadius = "50px";
+_bg__login.style.background = "#";
+_bg__login.style.background = "linear-gradient(-180deg , rgba(0,89,139,1) 0%, rgba(173,104,104,1) 100%)";
+
+content_container.appendChild(_bg__login);
+
+var widget = document.createElement("div");
+widget.id = "widget";
+widget.style.left = "40px";
+widget.style.top = "40px";
+widget.style.width = "280px";
+widget.style.height = "700px";
+widget.style.background = 'rgba(188.35,188.35,188.35,1)';
+
+content_container.appendChild(widget);
+
+var header = document.createElement("div");
+header.innerHTML = "KaraCare <br/>EMERGENCY SYSTEM";
+header.style.fontWeight = "bold";
+header.style.textAlign = "center";
+header.id = "header";
+header.style.left = "123px";
+header.style.top = "112px";
+header.style.width = "177px";
+header.style.height = "53.6px";
+header.style.fontFamily = "Inter";
+header.style.fontSize = "16px";
+header.style.overflow = "hidden";
+header.style.color = "#000000";
+
+content_container.appendChild(header);
+
+var logo = document.createElement("img");
+logo.id = "logo";
+logo.style.left = "79px";
+logo.style.top = "105px";
+logo.style.width = "68px";
+logo.style.height = "66px";
+logo.src = "skins/logo.png";
+
+content_container.appendChild(logo);
+
+var border = document.createElement("img");
+border.id = "border";
+border.style.left = "40px";
+border.style.top = "190px";
+border.style.width = "280px";
+border.style.height = "48px";
+border.src = "skins/border.png";
+
+content_container.appendChild(border);
+
+var fieldconf = document.createElement("div");
+fieldconf.id = "fieldconf";
+fieldconf.style.left = "126px";
+fieldconf.style.top = "433px";
+fieldconf.style.width = "51px";
+fieldconf.style.height = "21px";
+fieldconf.style.borderRadius = "100px";
+fieldconf.style.background = 'rgba(217,217,217,1)';
+
+content_container.appendChild(fieldconf);
+
+var fielddel = document.createElement("div");
+fielddel.id = "fielddel";
+fielddel.style.left = "186px";
+fielddel.style.top = "433px";
+fielddel.style.width = "51px";
+fielddel.style.height = "21px";
+fielddel.style.borderRadius = "100px";
+fielddel.style.background = 'rgba(69.8,82.48,94.19,1)';
+
+content_container.appendChild(fielddel);
+
+var emerheader = document.createElement("div");
+emerheader.innerHTML = "LOGIN";
+emerheader.style.fontWeight = "bold";
+emerheader.style.textAlign = "center";
+emerheader.id = "emerheader";
+emerheader.style.left = "102px";
+emerheader.style.top = "202px";
+emerheader.style.width = "159px";
+emerheader.style.height = "30.5px";
+emerheader.style.fontFamily = "Inter";
+emerheader.style.fontSize = "25px";
+emerheader.style.overflow = "hidden";
+emerheader.style.color = "#FFFCFC";
+
+content_container.appendChild(emerheader);
+
+var border2 = document.createElement("img");
+border2.id = "border2";
+border2.style.left = "40px";
+border2.style.top = "685px";
+border2.style.width = "280px";
+border2.style.height = "56px";
+border2.src = "skins/border2.png";
+
+content_container.appendChild(border2);
+
+var _delconf = document.createElement("div");
+_delconf.innerHTML = "Log In";
+_delconf.style.fontWeight = "bold";
+_delconf.style.textAlign = "center";
+_delconf.id = "_delconf";
+_delconf.style.left = "126px";
+_delconf.style.top = "435px";
+_delconf.style.width = "52px";
+_delconf.style.height = "22px";
+_delconf.style.fontFamily = "Inter";
+_delconf.style.fontSize = "10px";
+_delconf.style.overflow = "hidden";
+_delconf.style.color = "#000000";
+
+content_container.appendChild(_delconf);
+
+_delconf.style.cursor = "pointer";
+_delconf.onclick = (e) => {
+	@page_view("home_page");
+}
+
+var deltext = document.createElement("div");
+deltext.innerHTML = "Sign Up";
+deltext.style.fontWeight = "bold";
+deltext.style.textAlign = "center";
+deltext.id = "deltext";
+deltext.style.left = "186px";
+deltext.style.top = "435px";
+deltext.style.width = "52px";
+deltext.style.height = "22px";
+deltext.style.fontFamily = "Inter";
+deltext.style.fontSize = "10px";
+deltext.style.overflow = "hidden";
+deltext.style.color = "#D9D9D9";
+
+content_container.appendChild(deltext);
+
+var border2_ek1 = document.createElement("img");
+border2_ek1.id = "border2_ek1";
+border2_ek1.style.left = "39px";
+border2_ek1.style.top = "39px";
+border2_ek1.style.width = "288px";
+border2_ek1.style.height = "49px";
+border2_ek1.src = "skins/border2_ek1.png";
+
+content_container.appendChild(border2_ek1);
+
+var user = document.createElement("div");
+user.id = "user";
+user.style.width = "247px";
+user.style.height = "37px";
+user.style.left = "58px";
+user.style.top = "302px";
+user.style.position = "absolute";
+content_container.appendChild(user);
+
+var _bg__user_ek1 = document.createElement("div");
+_bg__user_ek1.id = "_bg__user_ek1";
+_bg__user_ek1.style.left = "0px";
+_bg__user_ek1.style.top = "0px";
+_bg__user_ek1.style.width = "247px";
+_bg__user_ek1.style.height = "37px";
+_bg__user_ek1.style.background = "#";
+
+user.appendChild(_bg__user_ek1);
+
+var box = document.createElement("div");
+box.id = "box";
+box.style.left = "-1px";
+box.style.top = "-1px";
+box.style.width = "247px";
+box.style.height = "37px";
+box.style.borderRadius = "4px";
+box.style.border = "1px solid #d5d8de";
+box.style.background = 'rgba(255,255,255,1)';
+
+user.appendChild(box);
+
+var key_field = document.createElement("div");
+key_field.innerHTML = "Username";
+key_field.style.textAlign = "left";
+key_field.id = "key_field";
+key_field.style.left = "16px";
+key_field.style.opacity = "0.8";
+key_field.style.filter = "alpha(opacity='80')";
+key_field.style.top = "11.77px";
+key_field.style.width = "91.6px";
+key_field.style.height = "22.6px";
+key_field.style.fontFamily = "PT Mono";
+key_field.style.fontSize = "16px";
+key_field.style.overflow = "hidden";
+key_field.style.color = "#323940";
+
+user.appendChild(key_field);
+
+var pass = document.createElement("div");
+pass.id = "pass";
+pass.style.width = "247px";
+pass.style.height = "37px";
+pass.style.left = "58px";
+pass.style.top = "353px";
+pass.style.position = "absolute";
+content_container.appendChild(pass);
+
+var _bg__pass_ek1 = document.createElement("div");
+_bg__pass_ek1.id = "_bg__pass_ek1";
+_bg__pass_ek1.style.left = "0px";
+_bg__pass_ek1.style.top = "0px";
+_bg__pass_ek1.style.width = "247px";
+_bg__pass_ek1.style.height = "37px";
+_bg__pass_ek1.style.background = "#";
+
+pass.appendChild(_bg__pass_ek1);
+
+var box_ek1 = document.createElement("div");
+box_ek1.id = "box_ek1";
+box_ek1.style.left = "-1px";
+box_ek1.style.top = "-1px";
+box_ek1.style.width = "247px";
+box_ek1.style.height = "37px";
+box_ek1.style.borderRadius = "4px";
+box_ek1.style.border = "1px solid #d5d8de";
+box_ek1.style.background = 'rgba(255,255,255,1)';
+
+pass.appendChild(box_ek1);
+
+var key_field_ek1 = document.createElement("div");
+key_field_ek1.innerHTML = "Password";
+key_field_ek1.style.textAlign = "left";
+key_field_ek1.id = "key_field_ek1";
+key_field_ek1.style.left = "16px";
+key_field_ek1.style.opacity = "0.8";
+key_field_ek1.style.filter = "alpha(opacity='80')";
+key_field_ek1.style.top = "11.77px";
+key_field_ek1.style.width = "91.6px";
+key_field_ek1.style.height = "22.6px";
+key_field_ek1.style.fontFamily = "PT Mono";
+key_field_ek1.style.fontSize = "16px";
+key_field_ek1.style.overflow = "hidden";
+key_field_ek1.style.color = "#323940";
+
+pass.appendChild(key_field_ek1);
+
+var widget_ek1 = document.createElement("div");
+widget_ek1.id = "widget_ek1";
+widget_ek1.style.left = "40px";
+widget_ek1.style.top = "40px";
+widget_ek1.style.width = "280px";
+widget_ek1.style.height = "700px";
+widget_ek1.style.background = 'rgba(188.35,188.35,188.35,1)';
+
+content_container.appendChild(widget_ek1);
+
+var header_ek1 = document.createElement("div");
+header_ek1.innerHTML = "KaraCare <br/>EMERGENCY SYSTEM";
+header_ek1.style.fontWeight = "bold";
+header_ek1.style.textAlign = "center";
+header_ek1.id = "header_ek1";
+header_ek1.style.left = "123px";
+header_ek1.style.top = "112px";
+header_ek1.style.width = "177px";
+header_ek1.style.height = "53.6px";
+header_ek1.style.fontFamily = "Inter";
+header_ek1.style.fontSize = "16px";
+header_ek1.style.overflow = "hidden";
+header_ek1.style.color = "#000000";
+
+content_container.appendChild(header_ek1);
+
+var logo_ek1 = document.createElement("img");
+logo_ek1.id = "logo_ek1";
+logo_ek1.style.left = "79px";
+logo_ek1.style.top = "105px";
+logo_ek1.style.width = "68px";
+logo_ek1.style.height = "66px";
+logo_ek1.src = "skins/logo_ek1.png";
+
+content_container.appendChild(logo_ek1);
+
+var border_ek1 = document.createElement("img");
+border_ek1.id = "border_ek1";
+border_ek1.style.left = "40px";
+border_ek1.style.top = "190px";
+border_ek1.style.width = "280px";
+border_ek1.style.height = "48px";
+border_ek1.src = "skins/border_ek1.png";
+
+content_container.appendChild(border_ek1);
+
+var fieldconf_ek1 = document.createElement("div");
+fieldconf_ek1.id = "fieldconf_ek1";
+fieldconf_ek1.style.left = "126px";
+fieldconf_ek1.style.top = "433px";
+fieldconf_ek1.style.width = "51px";
+fieldconf_ek1.style.height = "21px";
+fieldconf_ek1.style.borderRadius = "100px";
+fieldconf_ek1.style.background = 'rgba(217,217,217,1)';
+
+content_container.appendChild(fieldconf_ek1);
+
+var fielddel_ek1 = document.createElement("div");
+fielddel_ek1.id = "fielddel_ek1";
+fielddel_ek1.style.left = "186px";
+fielddel_ek1.style.top = "433px";
+fielddel_ek1.style.width = "51px";
+fielddel_ek1.style.height = "21px";
+fielddel_ek1.style.borderRadius = "100px";
+fielddel_ek1.style.background = 'rgba(69.8,82.48,94.19,1)';
+
+content_container.appendChild(fielddel_ek1);
+
+var emerheader_ek1 = document.createElement("div");
+emerheader_ek1.innerHTML = "LOGIN";
+emerheader_ek1.style.fontWeight = "bold";
+emerheader_ek1.style.textAlign = "center";
+emerheader_ek1.id = "emerheader_ek1";
+emerheader_ek1.style.left = "102px";
+emerheader_ek1.style.top = "202px";
+emerheader_ek1.style.width = "159px";
+emerheader_ek1.style.height = "30.5px";
+emerheader_ek1.style.fontFamily = "Inter";
+emerheader_ek1.style.fontSize = "25px";
+emerheader_ek1.style.overflow = "hidden";
+emerheader_ek1.style.color = "#FFFCFC";
+
+content_container.appendChild(emerheader_ek1);
+
+var border2_ek2 = document.createElement("img");
+border2_ek2.id = "border2_ek2";
+border2_ek2.style.left = "40px";
+border2_ek2.style.top = "685px";
+border2_ek2.style.width = "280px";
+border2_ek2.style.height = "56px";
+border2_ek2.src = "skins/border2_ek2.png";
+
+content_container.appendChild(border2_ek2);
+
+var _delconf_ek1 = document.createElement("div");
+_delconf_ek1.innerHTML = "Log In";
+_delconf_ek1.style.fontWeight = "bold";
+_delconf_ek1.style.textAlign = "center";
+_delconf_ek1.id = "_delconf_ek1";
+_delconf_ek1.style.left = "126px";
+_delconf_ek1.style.top = "435px";
+_delconf_ek1.style.width = "52px";
+_delconf_ek1.style.height = "22px";
+_delconf_ek1.style.fontFamily = "Inter";
+_delconf_ek1.style.fontSize = "10px";
+_delconf_ek1.style.overflow = "hidden";
+_delconf_ek1.style.color = "#000000";
+
+content_container.appendChild(_delconf_ek1);
+
+_delconf_ek1.style.cursor = "pointer";
+_delconf_ek1.onclick = (e) => {
+	@page_view("home_page");
+}
+
+var deltext_ek1 = document.createElement("div");
+deltext_ek1.innerHTML = "Sign Up";
+deltext_ek1.style.fontWeight = "bold";
+deltext_ek1.style.textAlign = "center";
+deltext_ek1.id = "deltext_ek1";
+deltext_ek1.style.left = "186px";
+deltext_ek1.style.top = "435px";
+deltext_ek1.style.width = "52px";
+deltext_ek1.style.height = "22px";
+deltext_ek1.style.fontFamily = "Inter";
+deltext_ek1.style.fontSize = "10px";
+deltext_ek1.style.overflow = "hidden";
+deltext_ek1.style.color = "#D9D9D9";
+
+content_container.appendChild(deltext_ek1);
+
+var border2_ek3 = document.createElement("img");
+border2_ek3.id = "border2_ek3";
+border2_ek3.style.left = "39px";
+border2_ek3.style.top = "39px";
+border2_ek3.style.width = "288px";
+border2_ek3.style.height = "49px";
+border2_ek3.src = "skins/border2_ek3.png";
+
+content_container.appendChild(border2_ek3);
+
+var user_ek2 = document.createElement("div");
+user_ek2.id = "user_ek2";
+user_ek2.style.width = "247px";
+user_ek2.style.height = "37px";
+user_ek2.style.left = "58px";
+user_ek2.style.top = "302px";
+user_ek2.style.position = "absolute";
+content_container.appendChild(user_ek2);
+
+var _bg__user_ek3 = document.createElement("div");
+_bg__user_ek3.id = "_bg__user_ek3";
+_bg__user_ek3.style.left = "0px";
+_bg__user_ek3.style.top = "0px";
+_bg__user_ek3.style.width = "247px";
+_bg__user_ek3.style.height = "37px";
+_bg__user_ek3.style.background = "#";
+
+user_ek2.appendChild(_bg__user_ek3);
+
+var box_ek2 = document.createElement("div");
+box_ek2.id = "box_ek2";
+box_ek2.style.left = "-1px";
+box_ek2.style.top = "-1px";
+box_ek2.style.width = "247px";
+box_ek2.style.height = "37px";
+box_ek2.style.borderRadius = "4px";
+box_ek2.style.border = "1px solid #d5d8de";
+box_ek2.style.background = 'rgba(255,255,255,1)';
+
+user_ek2.appendChild(box_ek2);
+
+var key_field_ek2 = document.createElement("div");
+key_field_ek2.innerHTML = "Username";
+key_field_ek2.style.textAlign = "left";
+key_field_ek2.id = "key_field_ek2";
+key_field_ek2.style.left = "16px";
+key_field_ek2.style.opacity = "0.8";
+key_field_ek2.style.filter = "alpha(opacity='80')";
+key_field_ek2.style.top = "11.77px";
+key_field_ek2.style.width = "91.6px";
+key_field_ek2.style.height = "22.6px";
+key_field_ek2.style.fontFamily = "PT Mono";
+key_field_ek2.style.fontSize = "16px";
+key_field_ek2.style.overflow = "hidden";
+key_field_ek2.style.color = "#323940";
+
+user_ek2.appendChild(key_field_ek2);
+
+var pass_ek2 = document.createElement("div");
+pass_ek2.id = "pass_ek2";
+pass_ek2.style.width = "247px";
+pass_ek2.style.height = "37px";
+pass_ek2.style.left = "58px";
+pass_ek2.style.top = "353px";
+pass_ek2.style.position = "absolute";
+content_container.appendChild(pass_ek2);
+
+var _bg__pass_ek3 = document.createElement("div");
+_bg__pass_ek3.id = "_bg__pass_ek3";
+_bg__pass_ek3.style.left = "0px";
+_bg__pass_ek3.style.top = "0px";
+_bg__pass_ek3.style.width = "247px";
+_bg__pass_ek3.style.height = "37px";
+_bg__pass_ek3.style.background = "#";
+
+pass_ek2.appendChild(_bg__pass_ek3);
+
+var box_ek3 = document.createElement("div");
+box_ek3.id = "box_ek3";
+box_ek3.style.left = "-1px";
+box_ek3.style.top = "-1px";
+box_ek3.style.width = "247px";
+box_ek3.style.height = "37px";
+box_ek3.style.borderRadius = "4px";
+box_ek3.style.border = "1px solid #d5d8de";
+box_ek3.style.background = 'rgba(255,255,255,1)';
+
+pass_ek2.appendChild(box_ek3);
+
+var key_field_ek3 = document.createElement("div");
+key_field_ek3.innerHTML = "Password";
+key_field_ek3.style.textAlign = "left";
+key_field_ek3.id = "key_field_ek3";
+key_field_ek3.style.left = "16px";
+key_field_ek3.style.opacity = "0.8";
+key_field_ek3.style.filter = "alpha(opacity='80')";
+key_field_ek3.style.top = "11.77px";
+key_field_ek3.style.width = "91.6px";
+key_field_ek3.style.height = "22.6px";
+key_field_ek3.style.fontFamily = "PT Mono";
+key_field_ek3.style.fontSize = "16px";
+key_field_ek3.style.overflow = "hidden";
+key_field_ek3.style.color = "#323940";
+
+pass_ek2.appendChild(key_field_ek3);
+
