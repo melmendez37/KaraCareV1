a68d4a5919aaae70fc72b41ba8e73270bba97460
timmutia <timothyrobertmutia@gmail.com>
2023-12-01 12:28:56 +0800

    centered the inpatient and outpatient pages

--- screens/Inpatient.js
+++ screens/Inpatient.js
@@ -1,4 +1,4 @@
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet, TextInput } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 
@@ -7,32 +7,32 @@ const Inpatient = () => {
     <View style={styles.container}>
         <LinearGradient colors={['#00598B', '#AD6868']} style={styles.s1}>
             <View style={styles.s2} />
-            <Text style={styles.s3}> KaraCare EMERGENCY SYSTEM </Text>
+            <Text style={styles.title}> KaraCare EMERGENCY SYSTEM </Text>
             <Image style={styles.s4} source= {require('../assets/karacare.png')} />
-            <View style={styles.s5} />
-            <View style={styles.s6} />
+            <View style={styles.confirmButton} />
+            <View style={styles.deleteButton} />
             <View style={styles.s7}></View>
-            <Text style={styles.s8}> Confirm </Text>
-            <Text style={styles.s9}> Delete </Text>
+            <Text style={styles.confirmText}> Confirm </Text>
+            <Text style={styles.deleteText}> Delete </Text>
             <View style={styles.s10}></View>
-            <Text style={styles.s11}> INPATIENT </Text>
+            <Text style={styles.inpatientText}> INPATIENT </Text>
             <View style={styles.s12}></View>
             <View style={styles.s13}></View>
-            <View style={styles.s14}>
-                <View style={styles.s15} />
-                <Text style={styles.s16}> Name </Text>
+            <View style={styles.container1}>
+                <View style={styles.textBox1} />
+                <TextInput placeholder='Name' style={styles.input1}></TextInput>
             </View>
-            <View style={styles.s17}>
-                <View style={styles.s18} />
-                <Text style={styles.s19}> Room Number </Text>
+            <View style={styles.container2}>
+                <View style={styles.textBox2} />
+                <TextInput placeholder='Room Number' style={styles.input2}></TextInput>
             </View>
-            <View style={styles.s20}>
-                <View style={styles.s21} />
-                <Text style={styles.s22}> Phone Number </Text>
+            <View style={styles.container3}>
+                <View style={styles.textBox3} />
+                <TextInput placeholder='Phone Number' style={styles.input3}></TextInput>
             </View>
-            <View style={styles.s23}>
-                <View style={styles.s24} />
-                <Text style={styles.s25}> MD Assigned </Text>
+            <View style={styles.container4}>
+                <View style={styles.textBox4} />
+                <TextInput placeholder='MS Assigned' style={styles.input4}></TextInput>
             </View>
         </LinearGradient>
     </View>
@@ -43,29 +43,29 @@ const Inpatient = () => {
 const styles = StyleSheet.create({
     container: {width: 424, left: 0, top: 0, flex: 1, alignItems: 'center',justifyContent: 'center', backgroundColor: 'black'},
     s1: {width: '100%', height: '100%', position: 'relative', borderRadius: 50, overflow: 'hidden'},
-    s2:{width: 280, height: 700, left: 40, top: 41, position: 'absolute', backgroundColor: '#BCBCBC'},
-    s3:{width: 248, height: 49, left: 94, top: 122, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '700'},
+    s2:{width: 340, height: 700, left: 40, top: 41, position: 'absolute', backgroundColor: '#BCBCBC'},
+    title:{width: 248, height: 49, left: 130, top: 122, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '700'},
     s4:{width: 68, height: 66, left: 60, top: 105, position: 'absolute'},
-    s5:{width: 51, height: 21, left: 119, top: 476, position: 'absolute', backgroundColor: '#D9D9D9', borderRadius: 100},
-    s6:{width: 51, height: 21, left: 192, top: 476, position: 'absolute', backgroundColor: '#506C86', borderRadius: 100},
-    s7:{width: 280, height: 56, left: 40, top: 629, position: 'absolute', backgroundColor: '#8C8C8C'},
-    s8:{width: 46, height: 19, left: 122, top: 478, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 10, fontFamily: 'Inter', fontWeight: '700'},
-    s9:{width: 46, height: 19, left: 195, top: 478, position: 'absolute', textAlign: 'center', color: '#D9D9D9', fontSize: 10, fontFamily: 'Inter', fontWeight: '700',},
-    s10:{width: 281, height: 48, left: 40, top: 186, position: 'absolute', backgroundColor: '#46525E'},
-    s11:{width: 237, height: 23, left: 64, top: 198, position: 'absolute', textAlign: 'center', color: '#FFFBFB', fontSize: 20, fontFamily: 'Inter', fontWeight: '700', },
-    s12:{width: 280, height: 41, left: 40, top: 41, position: 'absolute', backgroundColor: '#46525E'},
-    s13:{width: 281, height: 56, left: 40, top: 685, position: 'absolute', backgroundColor: '#46525E'},
-    s14:{width: 247, height: 37, left: 57, top: 254, position: 'absolute'},
-    s15:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
-    s16:{left: 16, top: 11.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400', },
-    s17:{width: 247, height: 37, left: 57, top: 306, position: 'absolute'},
-    s18:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
-    s19:{left: 16, top: 11.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400', },
-    s20:{width: 247, height: 37, left: 57, top: 358, position: 'absolute'},
-    s21:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
-    s22:{left: 16, top: 11.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400',},
-    s23:{width: 247, height: 37, left: 57, top: 410, position: 'absolute'},
-    s24:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
-    s25:{left: 16, top: 11.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400', }
+    confirmButton:{width: 65, height: 21, left: 147, top: 476, position: 'absolute', backgroundColor: '#D9D9D9', borderRadius: 100},
+    deleteButton:{width: 65, height: 21, left: 220, top: 476, position: 'absolute', backgroundColor: '#506C86', borderRadius: 100},
+    s7:{width: 340, height: 56, left: 40, top: 629, position: 'absolute', backgroundColor: '#8C8C8C'},
+    confirmText:{width: 58, height: 19, left: 148, top: 478, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 10, fontFamily: 'Inter', fontWeight: '700'},
+    deleteText:{width: 50, height: 19, left: 226, top: 478, position: 'absolute', textAlign: 'center', color: '#D9D9D9', fontSize: 10, fontFamily: 'Inter', fontWeight: '700',},
+    s10:{width: 341, height: 48, left: 40, top: 186, position: 'absolute', backgroundColor: '#46525E'},
+    inpatientText:{width: 237, height: 30, left: 87, top: 198, position: 'absolute', textAlign: 'center', color: '#FFFBFB', fontSize: 20, fontFamily: 'Inter', fontWeight: '700', },
+    s12:{width: 340, height: 41, left: 40, top: 41, position: 'absolute', backgroundColor: '#46525E'},
+    s13:{width: 341, height: 56, left: 40, top: 685, position: 'absolute', backgroundColor: '#46525E'},
+    container1:{width: 247, height: 37, left: 87, top: 254, position: 'absolute'},
+    textBox1:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
+    input1:{left: 16, top: 7.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400', },
+    container2:{width: 247, height: 37, left: 87, top: 306, position: 'absolute'},
+    textBox2:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
+    input2:{left: 16, top: 7.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400', },
+    container3:{width: 247, height: 37, left: 87, top: 358, position: 'absolute'},
+    textBox3:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
+    input3:{left: 16, top: 7.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400',},
+    container4:{width: 247, height: 37, left: 87, top: 410, position: 'absolute'},
+    textBox4:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
+    input4:{left: 16, top: 7.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400', }
 })
 export default Inpatient
--- screens/Outpatient.js
+++ screens/Outpatient.js
@@ -10,9 +10,9 @@ const Outpatient = () => {
         <View style={styles.recOne} />
         <Text style={styles.title}>KaraCare EMERGENCY SYSTEM</Text>
         <Image style={styles.image} source= {require("../assets/karacare.png")} />
-        <View style={styles.recTwo}/>
         <View style={styles.button1}/>
-        <View style={styles.button2}></View>
+        <View style={styles.button2}/>
+        <View style={styles.recTwo}></View>
         <Text style={styles.buttonText2}>Confirm</Text>
         <Text style={styles.buttonText1}>Delete</Text>
         <View style={styles.recThree}></View>
@@ -41,25 +41,25 @@ const Outpatient = () => {
 const styles = StyleSheet.create({
     container: {width: 424, left: 0, top: 0, flex: 1, alignItems: 'center',justifyContent: 'center', backgroundColorColor: 'black'},
     something:{width: '100%', height: '100%', position: 'relative', borderRadius: 50, overflow: 'hidden'},
-    recOne:{width: 280, height: 700, left: 40, top: 41, position: 'absolute', backgroundColor: '#BCBCBC'},
-    title:{width: 248, height: 49, left: 95, top: 121, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '700'},
+    recOne:{width: 340, height: 700, left: 40, top: 41, position: 'absolute', backgroundColor: '#BCBCBC'},
+    title:{width: 248, height: 49, left:130, top: 121, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '700'},
     image:{width: 68, height: 66, left: 58, top: 107, position: 'absolute'},
-    recTwo:{width: 51, height: 21, left: 119, top: 420, position: 'absolute', backgroundColor: '#D9D9D9', borderRadius: 100},
-    button1:{width: 51, height: 21, left: 190, top: 420, position: 'absolute', backgroundColor: '#506C86', borderRadius: 100},
-    button2:{width: 280, height: 56, left: 40, top: 629, position: 'absolute', backgroundColor: '#8C8C8C'},
-    buttonText2:{width: 46, height: 19, left: 122, top: 422, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 10, fontFamily: 'Inter', fontWeight: '700'},
-    buttonText1:{width: 46, height: 19, left: 193, top: 421, position: 'absolute', textAlign: 'center', color: '#D9D9D9', fontSize: 10, fontFamily: 'Inter', fontWeight: '700'},
-    recThree:{width: 281, height: 56, left: 40, top: 685, position: 'absolute', backgroundColor: '#46525E'},
-    recFour:{width: 280, height: 41, left: 40, top: 41, position: 'absolute', backgroundColor: '#46525E'},
-    recFive:{width: 281, height: 48, left: 40, top: 185, position: 'absolute', backgroundColor: '#46525E'},
-    recText1:{width: 237, height: 23, left: 61, top: 197, position: 'absolute', textAlign: 'center', color: '#FFFBFB', fontSize: 20, fontFamily: 'Inter', fontWeight: '700'},
-    container2:{width: 247, height: 37, left: 57, top: 254, position: 'absolute'},
+    button1:{width: 65, height: 21, left: 150, top: 420, position: 'absolute', backgroundColor: '#D9D9D9', borderRadius: 100},
+    button2:{width: 65, height: 21, left: 230, top: 420, position: 'absolute', backgroundColor: '#506C86', borderRadius: 100},
+    recTwo:{width: 340, height: 56, left: 40, top: 629, position: 'absolute', backgroundColor: '#8C8C8C'},
+    buttonText2:{width: 55, height: 19, left: 154, top: 422, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 10, fontFamily: 'Inter', fontWeight: '700'},
+    buttonText1:{width: 46, height: 19, left: 238, top: 421, position: 'absolute', textAlign: 'center', color: '#D9D9D9', fontSize: 10, fontFamily: 'Inter', fontWeight: '700'},
+    recThree:{width: 341, height: 56, left: 40, top: 685, position: 'absolute', backgroundColor: '#46525E'},
+    recFour:{width: 340, height: 41, left: 40, top: 41, position: 'absolute', backgroundColor: '#46525E'},
+    recFive:{width: 341, height: 48, left: 40, top: 185, position: 'absolute', backgroundColor: '#46525E'},
+    recText1:{width: 237, height: 30, left: 92, top: 197, position: 'absolute', textAlign: 'center', color: '#FFFBFB', fontSize: 20, fontFamily: 'Inter', fontWeight: '700'},
+    container2:{width: 247, height: 37, left: 86, top: 254, position: 'absolute'},
     textBox1:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
     inputText1:{left: 16, top: 11.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400'},
-    container3:{width: 247, height: 37, left: 57, top: 306, position: 'absolute'},
+    container3:{width: 247, height: 37, left: 86, top: 306, position: 'absolute'},
     textBox2:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
     inputText2:{left: 16, top: 11.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400',},
-    container4:{width: 247, height: 37, left: 57, top: 358, position: 'absolute'},
+    container4:{width: 247, height: 37, left: 86, top: 358, position: 'absolute'},
     textBox3:{width: 247, height: 37, left: 0, top: 0, position: 'absolute', backgroundColor: 'white', borderRadius: 4, border: '1px #D5D8DE solid'},
     inputText3:{left: 16, top: 11.77, position: 'absolute', opacity: 0.80, color: '#323941', fontSize: 16, fontFamily: 'PT Mono', fontWeight: '400',},
 
